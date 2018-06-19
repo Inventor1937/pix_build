68aa55dec489cdc0ba63b46df7e3d1e1f08ea748
Inventor1938 <ivan.kolev@mail.bg>
2018-06-19 03:49:21 +0300

    Revert "core: config: Use host bison if requested"
    This reverts commit fe39bbe0761fafff7e8a0e8229c413a85fbf0ce0.

--- core/config.mk
+++ core/config.mk
@@ -594,11 +594,7 @@ JACK := $(HOST_OUT_EXECUTABLES)/jack
 # To run bison from elsewhere you need to set up enviromental variable
 # BISON_PKGDATADIR.
 BISON_PKGDATADIR := $(PWD)/external/bison/data
-ifeq ($(USE_HOST_BISON),yes)
-BISON := bison
-else
 BISON := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/bison/bison
-endif
 YACC := $(BISON) -d
 BISON_DATA := $(wildcard external/bison/data/* external/bison/data/*/*)
 
